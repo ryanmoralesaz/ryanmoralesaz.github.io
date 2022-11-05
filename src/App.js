3dfba0488612460cb0ec47033a72e08a8f432179
Immanuel Ryan Morales <ryan-mini@Immanuels-Mac-mini.local>
2022-11-05 05:20:24 -0700

    init react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <text>Hello World</text>
+      </header>
+    </div>
+  );
+}
+
+export default App;
