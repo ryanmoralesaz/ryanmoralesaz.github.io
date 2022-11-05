a2333b8015efbe54627977589b9ee086d2234f13
Immanuel Ryan Morales <ryan-mini@Immanuels-Mac-mini.local>
2022-11-05 07:33:05 -0700

    added hello screen

--- src/App.js
+++ src/App.js
@@ -2,12 +2,45 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <text>hello</text>
+    <div style={styles.App}>
+      <header style={styles.appHeader}>
+        <div style={styles.textWrapper}>
+          <text style={styles.centerText}>{`Hello.\nMy name is \nRyan Morales.\nI will be uploading my portfolio\nand resume very soon.`}</text>
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+const styles = {
+  App: {
+    textAlign: "center",
+  },
+  appHeader: {
+    backgroundColor: "#443355",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    // fontSize: "calc(10px + 2vmin)",
+    // color: "white",
+  },
+  textWrapper: {
+    display: "flex",
+    border: "1px solid white",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    marginLeft: "20vw",
+    marginRight: "20vw",
+  },
+  centerText: {
+    color: "white",
+    fontSize: "5vh",
+    marginLeft: "5vw",
+    marginRight: "5vw",
+    whiteSpace: "pre-wrap",
+  },
+};
